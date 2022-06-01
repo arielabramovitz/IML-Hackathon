ecfb2d7b5dc1fda54eb9496dffe3e94594ce62bd
arielabramovitz <arielabramovitz7@gmail.com>
2022-06-01 20:59:23 +0300

    initial push

--- Model.py
+++ Model.py
@@ -1,5 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import SpectralClustering, KMeans
+from sklearn.decomposition import PCA
 
+import plotly.graph_objs as go
 
 
 if __name__ == "__main__":
