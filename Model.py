ad5c7412e53da5d8c425424d6587b6780703fbce
arielabramovitz <arielabramovitz7@gmail.com>
2022-06-02 11:49:12 +0300

    Updated Model.py

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 import typing
 
+from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cluster import SpectralClustering, KMeans
 from sklearn.decomposition import PCA
@@ -33,35 +34,50 @@ def compress_data(X: DataFrame, y: DataFrame):
     pass
 
 
+def find_optimal_hyperparameter(X: DataFrame, y: Series, estimator: BaseEstimator, ranges: list, cv,
+                                *hyperparameters):
+    for i in range(len(hyperparameters)):
+        alphas = np.linspace(ranges[i][0], ranges[i][1], cv)
+        kfold = KFold(n_splits=cv)
+        train_inds, test_inds = kfold.split(X, y)
+
+
 # ------------------------------------------------------ #
 
 def optimize_logistic_regression():
     """ Hyper-parameter optimization"""
     pass
 
+
 def optimize_nearest_neighbor():
     """ Hyper-parameter optimization"""
     pass
+
 
 def optimize_SVM():
     """ Hyper-parameter optimization"""
     pass
 
+
 def optimize_kernel_SVM():
     """ Hyper-parameter optimization"""
     pass
+
 
 def optimize_naive_base():
     """ Hyper-parameter optimization"""
     pass
 
+
 def optimize_decision_tree():
     """ Hyper-parameter optimization"""
     pass
 
+
 def optimize_random_forest():
     """ Hyper-parameter optimization"""
     pass
+
 
 # ------------------------------------------------------ #
 
@@ -71,6 +87,7 @@ def compare_learners(learner_list: list):
      returns the most successful one."""
     # todo draw a nice graph
     pass
+
 
 if __name__ == "__main__":
     np.random.seed(0)
